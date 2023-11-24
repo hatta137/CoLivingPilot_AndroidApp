0ca8ca681ccf62a0d03ee2b7b6b544b5e8d631c7
Kevin <Kevin@KEVIN-DESKTOP>
2023-11-24 23:34:54 +0100

    feat: add KeyValueStore for SharedPreferences

--- app/src/main/java/de/fhe/ai/colivingpilot/core/CoLiPiApplication.kt
+++ app/src/main/java/de/fhe/ai/colivingpilot/core/CoLiPiApplication.kt
@@ -1,12 +1,8 @@
 package de.fhe.ai.colivingpilot.core
 
 import android.app.Application
-import android.content.Context
 import android.util.Log
-import dagger.hilt.android.HiltAndroidApp
-import de.fhe.ai.colivingpilot.storage.WgDatabase
 
-@HiltAndroidApp
 class CoLiPiApplication : Application() {
 
     private lateinit var keyValueStore: KeyValueStore
@@ -14,7 +10,6 @@ class CoLiPiApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         keyValueStore = KeyValueStore(this)
-        instance = this
         Log.i(LOG_TAG, "Application initialized.")
     }
 
@@ -24,13 +19,6 @@ class CoLiPiApplication : Application() {
 
     companion object {
         const val LOG_TAG = "CoLiPi"
-        lateinit var instance: CoLiPiApplication
-            private set
-
-        fun applicationContext() : Context {
-            return instance.applicationContext
-        }
-
     }
 
 }
