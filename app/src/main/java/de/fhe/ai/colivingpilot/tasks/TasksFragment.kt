a8b51a150c920962705ea4b2ea908446d604258a
dario <dario.dassler@fh-erfurt.de>
2023-12-15 10:59:42 +0100

    Navbar is implemented #AppSteht!

---
+++ app/src/main/java/de/fhe/ai/colivingpilot/tasks/TasksFragment.kt
@@ -0,0 +1,36 @@
+package de.fhe.ai.colivingpilot.tasks
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import de.fhe.ai.colivingpilot.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TasksFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TasksFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tasks, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val text : TextView = view.findViewById(R.id.textView)
+        text.text = "Hendrik hasst Unity & Menschen & Android Studio"
+    }
+
+}
