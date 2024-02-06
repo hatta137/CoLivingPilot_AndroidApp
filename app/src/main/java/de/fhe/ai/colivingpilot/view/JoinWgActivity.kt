4af90fb67091b176a486e5bd62eb896d98a40a20
dario <dario.dassler@fh-erfurt.de>
2024-02-06 01:21:16 +0100

    add todo

--- app/src/main/java/de/fhe/ai/colivingpilot/view/JoinWgActivity.kt
+++ app/src/main/java/de/fhe/ai/colivingpilot/view/JoinWgActivity.kt
@@ -6,9 +6,11 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
+import com.google.zxing.integration.android.IntentIntegrator
 import de.fhe.ai.colivingpilot.MainActivity
 import de.fhe.ai.colivingpilot.R
 import de.fhe.ai.colivingpilot.core.CoLiPiApplication
@@ -23,9 +25,16 @@ class JoinWgActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_wg)
-        
+
         val scanQrBtn = findViewById<Button>(R.id.join_wg_activity_join_wg_activity_button_join_qr)
-        
+
+        scanQrBtn.setOnClickListener {
+            // Initiieren des QR-Code-Scanners
+            val integrator = IntentIntegrator(this)
+            integrator.setOrientationLocked(false)
+            integrator.initiateScan()
+        }
+
         val joinBtn = findViewById<Button>(R.id.join_wg_activity_button_join)
         joinBtn.setOnClickListener { 
             UiUtils.hideKeyboard(this)
@@ -69,6 +78,27 @@ class JoinWgActivity : AppCompatActivity() {
                     setFormLocked(false)
                 }
             })
+        }
+    }
+
+    //TODO auf neuere Varaiante umstellen!!!
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents != null) {
+                // Hier kannst du mit dem gescannten QR-Code-Inhalt arbeiten (z.B. anzeigen)
+                // result.contents enthält den gescannten Text
+                // Beispiel:
+                val codeField = findViewById<TextInputLayout>(R.id.join_wg_activity_textfield_code)
+                codeField.editText?.setText(result.contents)
+                Toast.makeText(this, "Gescannter QR-Code: ${result.contents}", Toast.LENGTH_LONG).show()
+            } else {
+                // Wenn der Scan abgebrochen wurde
+
+                Toast.makeText(this, "Scan abgebrochen", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
